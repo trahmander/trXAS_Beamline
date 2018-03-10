a4d3ee01c49280dee24045e658438c93d84abed2
Tahiyat Rahman <rahman.tahiyat@gmail.com>
2018-03-09 18:14:29 -0800

    got some shifting done, set the step size to 1 meV. need to do this for all peaks.

--- trXAS/trXAS_average.py
+++ trXAS/trXAS_average.py
@@ -8,29 +8,33 @@ __date__    = "2018-02-21"
 __credits__ = ["Johannes Mahl"]
 __email__ = "trahman@lbl.gov"
 __status__ = "production"
-__revision__= "2"
+__revision__= "3"
 ###############################################################################
 #Import modules
 ###############################################################################
 import os
 import sys
 import numpy as np
+import itertools as iter
 from matplotlib import pyplot as plt
-from scipy.interpolate import UnivariateSpline
-from scipy.interpolate import InterpolatedUnivariateSpline
+#from scipy.interpolate import UnivariateSpline
+#from scipy.interpolate import InterpolatedUnivariateSpline
 import scipy.interpolate as itp
+
+peaks = []
+peakAvgs = []
+splines = []
+lines= []
+stepSize = 0.001
 
 def get_data_files(path):
     pathName = path
     dataFiles = os.listdir(pathName)
-    dFiles = []
-#    print(dataFiles)
-#    print(len(dataFiles))
+    dFiles = []    
     for file in dataFiles:
-#        print(file)
         if  file.endswith("data.txt") and not( "average" in file) :
-            print(file)
-            dFiles.append(pathName+file)
+#            print(file)
+            dFiles.append(os.path.join(pathName,file))
     return dFiles 
 def load_file(fileName):
     dataSet = np.loadtxt(fileName, skiprows=1)
@@ -42,12 +46,13 @@ def load_file(fileName):
 def find_peak(xlow, xhigh, step, func):
     nearPeak = np.linspace(xhigh, xlow, step)
     feature = func(nearPeak)
-    maxVal = feature[0] 
-    peak = nearPeak[0]
+    maxVal = feature[0]  
+    peak = 0
     for i in range( len(feature) ):
         if feature[i] > maxVal:
             maxVal = feature[i]
             peak = i
+    peaks.append(nearPeak[peak])
     return nearPeak[peak]
 def get_spline(low, high, step, x, y):
     line = np.linspace(low, high, step)
@@ -55,9 +60,12 @@ def get_spline(low, high, step, x, y):
 #    splinE = itp.InterpolatedUnivariateSpline(photonE, yAllNorm)
 #    splinE = itp.LSQUnivariateSpline(photonE, yAllNorm)
 #    print( "photonE sorted? " + all( photonE[i] <= photonE[i+1] for i in range(len(photonE)-1) ) ) #check if list is really sorted
+
     spline = itp.interp1d(x, y, kind='slinear')                                   #This one does linear interpolation. kinda ugly
+    lines.append(line)
+    splines.append(spline)
     return line, spline
-def photonE_counts_plot(dataSet):
+def photonE_counts_plot(dataSet, file):
     (photonE, #SEphotonE,
         xRefNorm, #SExRefNorm,
         xRef, #SExRef,
@@ -91,16 +99,36 @@ def photonE_counts_plot(dataSet):
 #    highE = np.amax(photonE)
     lowE = photonE[0]
     highE = photonE[-1]
-    step = 1000
-    linE, splinE = get_spline(lowE, highE, step, photonE, yAllNorm)
-    firstPeak = find_peak(532, 537, 1000, splinE)             
-    figure = plt.figure(dpi=100)
-    plt.plot( photonE, yAllNorm, marker= 'd', linestyle='none' )
+    step = (highE - lowE) / stepSize
+    linE, splinE = get_spline(lowE, highE, step, photonE, xPumpNorm)
+    firstPeak = find_peak(532, 537, step, splinE)             
+#    figure = plt.figure(dpi=100)
+#    plt.title("File: "+str(file))
+    plt.plot( photonE, yPumpNorm, marker= 'd', linestyle='none' )
     plt.plot( linE, splinE(linE), linewidth=1 )
     plt.axvline( firstPeak, linewidth = 1 )
-    plt.show()
-#def shift_spline(spline, line, delta):
-#    spline(line) = spline(line+delta)
+#    plt.show()
+# NEEDS WORK. Find a way to convert peak difference into iter difference
+def shift_spline(splineNum):
+    vals = splines[splineNum]( lines[splineNum] )
+    ref = np.amin( peakAvgs )
+    
+    if ref == peakAvgs[splineNum]:
+        return vals, lines[splineNum]
+    else:
+        delta = peakAvgs[splineNum] - ref
+        index = int ( delta / stepSize )
+        for i in range (len(vals) - index ) :
+            vals[i] = vals[i+index]
+        return vals[:-index], lines[splineNum][:-index]
+def calc_step_size(vals):
+    pairs =  list ( iter.combinations(vals, 2) )
+    diff = [p[1] - p[0] for p in pairs]
+    diff = np.abs(diff)
+    step = round (np.amin(diff)/2, -3)
+    return step
+    
+        
 
 #def average_splines(splineSet, line):
 #    splineSum = np.zeros_like(line)
@@ -113,13 +141,51 @@ def photonE_counts_plot(dataSet):
 #    return splineAvg
 
 def main():
-    dir = os.path.dirname(__file__)
-    path = "trXAS data sample - date eval software/hv scans processed/0198_CuO_O_K-edge_355nm_58pc/"
-    dataFiles = get_data_files(path)
-    for file in dataFiles:
+#    dir = os.path.dirname(__file__)
+#    paths = ["trXAS data sample - date eval software/hv scans processed/0198_CuO_O_K-edge_355nm_58pc/",
+#             "trXAS data sample - date eval software/hv scans processed/0199_CuO_O_K-edge_355nm_58pc/",
+#             "trXAS data sample - date eval software/hv scans processed/0201_CuO_O_K-edge_355nm_58pc/",
+#             "trXAS data sample - date eval software/hv scans processed/0202_CuO_O_K-edge_355nm_58pc/"]
+#    path = "trXAS data sample - date eval software/hv scans processed/0198_CuO_O_K-edge_355nm_58pc/"
+    direct ="trXAS data sample - date eval software/hv scans processed/"
+    paths = os.listdir(direct)
+    for i in range( len(paths) ):
+        paths[i] = os.path.join(direct, paths[i])
+    for path in paths:
+         if "avg" in path:
+            paths.remove(path)
+    for path in paths :
+        dataFiles = get_data_files(path)
+        fig = plt.figure(dpi=100)
+#        for i in range(len(dataFiles)):
+        file = dataFiles[0]
         dataSet = load_file(file)
-        photonE_counts_plot(dataSet)
+        photonE_counts_plot(dataSet, file)
+        
+        print( peaks )
+        print( set(peaks) )
+        peakAvgs.append( np.average(peaks) )
+        peaks.clear()
+    print( peakAvgs )
+    
+    
+#    print( splines[0](lines[0]) )
+#    print( shiftedVals )
+#    for i in range ( len(splines) ) :
+#        shiftedVals, shiftedLine = shift_spline(i)
+#        fig = plt.figure(dpi=100)
+#        plt.plot(shiftedLine, shiftedVals, linewidth=1, color='b')
+#        plt.plot(lines[i], splines[i](lines[i]), linewidth=1,  color='g')
+    fig = plt.figure(dpi=100)
+    for i in range ( len(splines) ) :
+        shiftedVals, shiftedLine = shift_spline(i)
+        plt.plot(shiftedLine, shiftedVals, linewidth=1)
 
+    
+
+
+
+    
     
 if __name__ == "__main__":
     main()
